4f78c938f9288d91e480e8b7d0122caefadc6b1b
benvacha <ben@benvacha.com>
2020-03-27 01:12:39 -0600

    Find meta goto.

--- elements/paras/finder.vue
+++ elements/paras/finder.vue
@@ -5,16 +5,38 @@
 
   <div class="subbody">
   <div class="bodyer para">
+    <h1>GoTo Majig</h1>
+    <form @submit.prevent="goto(path)">
+      <InputText
+        v-model="path"
+        placeholder="/path"
+      />
+      <input type="submit"
+        value="GoTo" />
+    </form>
     <h1>Find Majig</h1>
     <form @submit.prevent="search">
-      <input type="submit"
-        value="Search" />
-      <h2>Keyword</h2>
       <InputText
         v-model="keyword"
         placeholder="keyword"
       />
+      <input type="submit"
+        value="Find" />
     </form>
+    <h2>Results
+      <span>{{majigs.length}}</span>
+    </h2>
+    <ul>
+      <li v-if="!majigs.length">
+        <div>No Results</div>
+      </li>
+      <li v-for="majig in majigs">
+        <a :click="goto(majig.path)">
+          <span>{{majig.markdown | previewed}}</span>
+          <h5>{{majig.path}}</h5>
+        </a>
+      </li>
+    </ul>
   </div>
   </div>
 
@@ -30,12 +52,33 @@ export default {
   components: {
     InputText,
   },
+  filters: {
+    previewed: (value) => {
+      const length = 90;
+      const index = value.indexOf(this.keyword);
+      let offset = length - keyword.length;
+      offset = Math.floor(offset / 2);
+      return value.substring(index - offset,
+        index + this.keyword.length + offset);
+    },
+  },
   data () {
     return {
+      path: '',
       keyword: '',
+      majigs: [],
     };
   },
   methods: {
+    goto (path) {
+      if(path[0] !== '/') {
+        path = '/' + path;
+      }
+      this.$emit('open', false);
+      this.$router.push(
+        path
+      ).catch(err => {});
+    },
     search () {
       if(!this.keyword) {
         return this.$emit('notify',
